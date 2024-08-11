7056acd89d25a51bc16d0a8d9ea89f9db1518f62
Galina Lukina <lukina.galina11@gmail.com>
2024-08-11 18:23:55 +0400

    add tests

--- src/classes.py
+++ src/classes.py
@@ -6,13 +6,30 @@ class Product:
     price: float
     quantity: int
 
-    def __init__(self, name: str, description: str, price: float, quantity: int):
+    def __init__(self, name, description, price, quantity):
         """Метод для инициализации экземпляра класса"""
 
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product_dict):
+        return cls(**product_dict)
+
+
+    @property
+    def price(self):
+        return self.__price
+
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
 
 
 class Category:
@@ -29,6 +46,32 @@ class Category:
 
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+
+    @property
+    def products(self):
+        return self.__products
+
+
+    @products.setter
+    def add_product(self, product: Product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+
+    @property
+    def products_list(self):
+        product_str = ""
+        for product in self.__products:
+            product_str += f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n'
+        return product_str
+
+
+product = {"name": "Nokia", "description": "Yellow", "price": 90000.0, "quantity": 10}
+new_product = Product.new_product(product)
+
+print(new_product.price)
+print(new_product.name)
--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,4 +1,4 @@
-from src.classes import Category
+from src.classes import Category, Product
 
 
 def test_product_init(product):
@@ -22,3 +22,42 @@ def test_category_init(category_1, category_2):
     assert category_1.product_count == 3
     assert category_2.product_count == 3
     assert Category.product_count == 3
+
+
+def test_category_products_list_property(category_1):
+    assert category_1.products_list == ("Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
+                                        "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n")
+
+
+def test_category_products_setter(category_1, product):
+    assert len(category_1.products) == 2
+    category_1.add_product = product
+    assert len(category_1.products) == 3
+
+
+def test_product_new_product():
+    product = {"name": "Nokia", "description": "Yellow", "price": 90000.0, "quantity": 10}
+    new_product = Product.new_product(product)
+    assert new_product.name == "Nokia"
+    assert new_product.description == "Yellow"
+    assert new_product.price == 90000.0
+    assert new_product.quantity == 10
+
+
+def test_price_property(product):
+    assert product.price == 180000.0
+
+
+def test_price_setter(capsys, product):
+    product.price = 0
+    message = capsys.readouterr()
+    assert message.out.strip() == "Цена не должна быть нулевая или отрицательная"
+    assert product.price == 180000.0
+
+    product.price = -99999.0
+    message = capsys.readouterr()
+    assert message.out.strip() == "Цена не должна быть нулевая или отрицательная"
+    assert product.price == 180000.0
+
+    product.price = 0.1
+    assert product.price == 0.1
