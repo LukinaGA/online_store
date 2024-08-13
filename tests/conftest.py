cfb02742fa35fbcc8e23e06c7353fca3d54bf8f4
Galina Lukina <lukina.galina11@gmail.com>
2024-08-13 20:13:34 +0400

    add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,11 +11,21 @@ def product():
 
 
 @pytest.fixture
+def product_2():
+    return Product(name="Iphone 15", description="512GB, Gray space", price=210000.0, quantity=8)
+
+
+@pytest.fixture
+def product_3():
+    return Product(name="Nokia", description="Yellow", price=90000.0, quantity=0)
+
+
+@pytest.fixture
 def category_1():
     return Category(
         name="Смартфоны",
         description="Смартфоны, как средство не только коммуникации, "
-        "но и получения дополнительных функций для удобства жизни",
+                    "но и получения дополнительных функций для удобства жизни",
         products=[
             Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5),
             Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
@@ -28,6 +38,6 @@ def category_2():
     return Category(
         name="Телевизоры",
         description="Современный телевизор, который позволяет наслаждаться просмотром, "
-        "станет вашим другом и помощником",
+                    "станет вашим другом и помощником",
         products=[Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)],
     )
